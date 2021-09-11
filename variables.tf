c3f646a311c6b209567ccf5aec245d1074fed006
karimarous <karimmaroous@gmail.com>
2021-09-11 14:49:09 +0100

    jenkins server

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable env {
+  type        = string
+  default     = "main"
+}
+
+variable image_id {
+  type        = string
+  default     = "ami-0dc2d3e4c0f9ebd18"
+}
+
+variable instance_type{
+    type      = string
+    default   = "t2.micro""
+}
