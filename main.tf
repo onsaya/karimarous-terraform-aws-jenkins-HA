abc7e27344c8820ad71af7ab5a9c2a01500b8a89
AYADI <ons.ayadi@hpe.com>
2021-09-14 23:34:46 +0200

    create jenkins

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "jenkins" {
     sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
     sudo yum upgrade
+    sudo amazon-linux-extras install epel -y
     sudo yum install jenkins java-1.8.0-openjdk-devel -y
     sudo systemctl daemon-reload
     sudo systemctl start jenkins
