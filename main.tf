dc25afa350aac2ce7f9369ec77d40e4e2a9ec210
AYADI <ons.ayadi@hpe.com>
2021-09-14 22:40:41 +0200

    create jenkins

--- main.tf
+++ main.tf
@@ -6,5 +6,16 @@ resource "aws_instance" "jenkins" {
   credit_specification {
     cpu_credits = "unlimited"
   }
- 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update –y",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum upgrade",
+      "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl start jenkins",
+      "sudo systemctl status jenkins"
+    ]
+  }
 }
