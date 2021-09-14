dcb6e13b46ebe7de6cedd67e93fda9b26e3e782a
AYADI <ons.ayadi@hpe.com>
2021-09-14 23:01:01 +0200

    create jenkins

--- main.tf
+++ main.tf
@@ -6,16 +6,15 @@ resource "aws_instance" "jenkins" {
   credit_specification {
     cpu_credits = "unlimited"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update –y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-      "sudo yum upgrade",
-      "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl start jenkins",
-      "sudo systemctl status jenkins"
-    ]
-  }
+  user_data=<<EOF
+    #! /bin/bash
+    sudo yum update –y
+    sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+    sudo yum upgrade
+    sudo yum install jenkins java-1.8.0-openjdk-devel -y
+    sudo systemctl daemon-reload
+    sudo systemctl start jenkins
+    sudo systemctl status jenkins
+  EOF
 }
