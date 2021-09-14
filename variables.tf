5e985627678a28de93a1b7267e49ed2dd4aebce0
AYADI <ons.ayadi@hpe.com>
2021-09-14 22:34:20 +0200

    create jenkins

--- variables.tf
+++ variables.tf
@@ -5,10 +5,11 @@ variable env {
 
 variable image_id {
   type        = string
-  default     = "ami-0dc2d3e4c0f9ebd18"
+  default     = "ami-087c17d1fe0178315"
 }
 
 variable instance_type{
     type      = string
     default   = "t2.micro"
 }
+
